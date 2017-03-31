82a01e0db01849b9a05ab05b51a575c5fb321d9b
Preston Matheson <pmphoto123@gmail.com>
2017-03-30 19:28:11 -0600

    added hours requested to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
-  validates_presence_of :date, :rationale
+  validates_presence_of :date, :rationale, :overtime_request
+
+  validates :overtime_request, numericality: { greater_than: 0.0 }
+
+  scope :posts_by, ->(user) {were(user_id: user.id)}
 end
