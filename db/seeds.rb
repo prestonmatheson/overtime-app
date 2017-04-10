dba642315a59a5f0cb2f39a64c9bae1375ffee1f
Preston Matheson <pmphoto123@gmail.com>
2017-04-10 15:37:18 -0600

    fixed bug with seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,29 @@
-AdminUser.create(email: "admin@test.com",
-                 password: "asdfasdf", 
-                 password_confirmation: "asdfasdf", 
-                 first_name: "Admin", 
-                 last_name: "Name",
-                 phone: "8017093615")
-
-@employee = Employee.create(email: "test@test.com", 
-                    password: "asdfasdf", 
-                    password_confirmation: "asdfasdf", 
-                    first_name: "Jon", 
-                    last_name: "Smith",
-                    phone: "8017093615")
-
-
-puts "1 Admin user created"
+@employee = Employee.create(email: "test@test.com",
+                            password: "asdfasdf",
+                            password_confirmation: "asdfasdf",
+                            first_name: "Jon",
+                            last_name: "Smith",
+                            phone: "8017093615")
 
 puts "1 employee created"
 
-AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
+AdminUser.create(email: "admin@test.com",
+                 password: "asdfasdf",
+                 password_confirmation: "asdfasdf",
+                 first_name: "Admin",
+                 last_name: "Name",
+                 phone: "8017093615")
 
+puts "1 Admin user created"
+
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
+    Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
 end
 
-puts "100 posts have been created"
+puts "100 Posts have been created"
